14c71ecba164d5b8c29596db1ef3629e27fb5d0e
thelampgod <virtual@riseup.net>
2021-02-05 17:47:47 +0100

    fix crash when checking neighbouring chunk

--- src/main/java/net/daporkchop/savesearcher/module/impl/BrokenPortalModule.java
+++ src/main/java/net/daporkchop/savesearcher/module/impl/BrokenPortalModule.java
@@ -2,14 +2,15 @@ package net.daporkchop.savesearcher.module.impl;
 
 import lombok.NonNull;
 import net.daporkchop.lib.math.vector.i.Vec3i;
+import net.daporkchop.lib.minecraft.region.util.NeighboringChunkProcessor;
 import net.daporkchop.lib.minecraft.registry.ResourceLocation;
+import net.daporkchop.lib.minecraft.util.BlockAccess;
 import net.daporkchop.lib.minecraft.world.Chunk;
-import net.daporkchop.lib.minecraft.world.Section;
 import net.daporkchop.lib.minecraft.world.World;
 import net.daporkchop.savesearcher.module.AbstractSearchModule;
 import net.daporkchop.savesearcher.output.OutputHandle;
 
-public final class BrokenPortalModule extends AbstractSearchModule<Vec3i> {
+public final class BrokenPortalModule extends AbstractSearchModule<Vec3i> implements NeighboringChunkProcessor {
 
     protected int portal_id;
     protected int obsidian_id;
@@ -26,46 +27,53 @@ public final class BrokenPortalModule extends AbstractSearchModule<Vec3i> {
     }
 
     @Override
-    protected void processChunk(Chunk chunk, OutputHandle handle) {
+    public void handle(long l, long l1, Chunk chunk) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void processChunk(@NonNull Chunk chunk, @NonNull OutputHandle handle) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handle(long current, long estimatedTotal, Chunk chunk, BlockAccess access) {
         final int portal = this.portal_id;
         final int obsidian = this.obsidian_id;
 
+        final int x = chunk.getX() << 4;
+        final int z = chunk.getZ() << 4;
 
-        for (int sectionY = 15; sectionY >= 0; sectionY--) {
-            Section section = chunk.section(sectionY);
-            if (section == null) {
-                continue;
-            }
-            for (int x = 15; x >= 0; x--) {
-                for (int y = 15; y >= 0; y--) {
-                    for (int z = 15; z >= 0; z--) {
-                        if (section.getBlockId(x, y, z) == portal) {
-                            int meta = section.getBlockMeta(x, y, z); // x = 1, z = 2, 0 = unknown?
+        for (int xx = 15; xx >= 0; xx--) {
+            for (int zz = 15; zz >= 0; zz--) {
+                for (int y = 255; y >= 0; y--) {
+                    if (access.getBlockId(x + xx, y, z + zz) == portal) {
+                        int meta = access.getBlockMeta(x + xx, y, z + zz); // x = 1, z = 2, 0 = unknown?
 
-                            switch (meta) {
-                                case 1:
-                                    int east_id = section.getBlockId(x + 1, y, z);
-                                    int west_id = section.getBlockId(x - 1, y, z);
+                        switch (meta) {
+                            case 1:
+                                int east_id = access.getBlockId(x + xx + 1, y, z + zz);
+                                int west_id = access.getBlockId(x + xx - 1, y, z + zz);
 
-                                    if ((east_id != portal
-                                            && east_id != obsidian)
-                                            || (west_id != portal
-                                            && west_id != obsidian)) {
-                                        handle.accept(new Vec3i(chunk.minX() + x, (section.getY() << 4) + y, chunk.minZ() + z));
-                                    }
-                                    break;
-                                case 2:
-                                    int south_id = section.getBlockId(x, y, z + 1);
-                                    int north_id = section.getBlockId(x, y, z - 1);
+                                if ((east_id != portal
+                                        && east_id != obsidian)
+                                        || (west_id != portal
+                                        && west_id != obsidian)) {
+                                    handle.accept(new Vec3i(x + xx, y, z + zz));
+                                }
+                                break;
+                            case 2:
+                                int south_id = access.getBlockId(x + xx, y, z + zz + 1);
+                                int north_id = access.getBlockId(x + xx, y, z + zz - 1);
 
-                                    if ((south_id != portal
-                                            && south_id != obsidian)
-                                            || (north_id != portal
-                                            && north_id != obsidian)) {
-                                        handle.accept(new Vec3i(chunk.minX() + x, (section.getY() << 4) + y, chunk.minZ() + z));
-                                    }
-                                    break;
-                            }
+                                if ((south_id != portal
+                                        && south_id != obsidian)
+                                        || (north_id != portal
+                                        && north_id != obsidian)) {
+                                    handle.accept(new Vec3i(x + xx, y, z + zz));
+                                }
+                                break;
+
                         }
                     }
                 }
